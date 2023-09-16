5705565af6f84a9ec04a52e98eb8ebf02a80cd43
alfredo215 <mc1121032018@unab.edu.sv>
2023-09-16 16:59:15 -0600

    tablas

--- src/Inicio.js
+++ src/Inicio.js
@@ -1,34 +1,36 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import Button from '@mui/material/Button';
 
 function Inicio() {
   const navigate = useNavigate();
   const [rentas, setRentas] = useState([]);
   const [usuarios, setUsuarios] = useState([]);
+  const [elementoAEliminar, setElementoAEliminar] = useState(null);
+  const [mostrarConfirmacion, setMostrarConfirmacion] = useState(false);
 
   useEffect(() => {
     // Obtener datos de rentas
-    fetch('http://localhost:3000/rentas', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: 'Bearer ' + localStorage.getItem('token'),
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => setRentas(data))
+    axios
+      .get('http://localhost:3000/rentas', {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + localStorage.getItem('token'),
+        },
+      })
+      .then((response) => setRentas(response.data))
       .catch((error) => console.log(error));
 
     // Obtener datos de usuarios
-    fetch('http://localhost:3000/usuarios', { // Cambia la URL aquí
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: 'Bearer ' + localStorage.getItem('token'),
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => setUsuarios(data))
+    axios
+      .get('http://localhost:3000/usuarios', {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + localStorage.getItem('token'),
+        },
+      })
+      .then((response) => setUsuarios(response.data))
       .catch((error) => console.log(error));
   }, []);
 
@@ -38,50 +40,106 @@ function Inicio() {
     return empleado ? empleado.nombre : 'Desconocido';
   };
 
+  // Función para eliminar un elemento
+  const eliminarElemento = () => {
+    axios
+      .delete(`http://localhost:3000/rentas/${elementoAEliminar}`, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + localStorage.getItem('token'),
+        },
+      })
+      .then(() => {
+        setRentas(rentas.filter((renta) => renta.rentaId !== elementoAEliminar));
+        setElementoAEliminar(null);
+        setMostrarConfirmacion(false);
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
-    <div className='container'>
+    <div className="container">
       <div>
         <h4>Listado de rentas</h4>
         {rentas.length === 0 && (
-          <h3 className='text-center alert alert-danger'>No hay datos para mostrar</h3>
+          <h3 className="text-center alert alert-danger">No hay datos para mostrar</h3>
         )}
         {rentas.length > 0 && (
-          <table className='table table-bordered table-dark'>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Cliente</th>
-                <th>Dui</th>
-                <th>Fecha</th>
-                <th>Automóvil</th>
-                <th>Placa</th>
-                <th>Color</th>
-                <th>Estado</th>
-                <th>Empleado</th>
-                <th>Acciones</th>
-              </tr>
-            </thead>
-            <tbody>
-              {rentas.length > 0 &&
-                rentas.map((renta) => (
-                  <tr key={renta.rentaId}>
-                    <td>{renta.rentaId}</td>
-                    <td>{renta.cliente}</td>
-                    <td>{renta.dui}</td>
-                    <td>{renta.fecha}</td>
-                    <td>{renta.carro_modelo}</td>
-                    <td>{renta.placa}</td>
-                    <td>{renta.color}</td>
-                    <td>{renta.estado}</td>
-                    <td>{getNombreEmpleado(renta.usuarioId_FK)}</td>
-                    <td>
-                                <button onClick={() => navigate(`/alquilerauto/${renta.rentaId}`)} className='btn btn-primary'>Editar</button>
-                                <button onClick={() => navigate(`/eliminar/${renta.rentaId}`)} className='btn btn-danger'>Eliminar</button>
-                            </td>
-                  </tr>
-                ))}
-            </tbody>
-          </table>
+          <div style={{ maxHeight: '400px', overflowY: 'scroll' }}>
+            <table className="table table-bordered table-dark">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Cliente</th>
+                  <th>Dui</th>
+                  <th>Fecha</th>
+                  <th>Automóvil</th>
+                  <th>Placa</th>
+                  <th>Color</th>
+                  <th>Estado</th>
+                  <th>Empleado</th>
+                  <th>Acciones</th>
+                </tr>
+              </thead>
+              <tbody>
+                {rentas.length > 0 &&
+                  rentas.map((renta) => (
+                    <tr key={renta.rentaId}>
+                      <td>{renta.rentaId}</td>
+                      <td>{renta.cliente}</td>
+                      <td>{renta.dui}</td>
+                      <td>{renta.fecha}</td>
+                      <td>{renta.carro_modelo}</td>
+                      <td>{renta.placa}</td>
+                      <td>{renta.color}</td>
+                      <td>{renta.estado}</td>
+                      <td>{getNombreEmpleado(renta.usuarioId_FK)}</td>
+                      <td>
+                        <Button
+                          onClick={() => navigate(`/alquilerauto/${renta.rentaId}`)}
+                          variant="contained"
+                          color="primary"
+                        >
+                          Editar
+                        </Button>
+                        <Button
+                          onClick={() => {
+                            setElementoAEliminar(renta.rentaId);
+                            setMostrarConfirmacion(true);
+                          }}
+                          variant="contained"
+                          color="error"
+                        >
+                          Eliminar
+                        </Button>
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+        {mostrarConfirmacion && (
+          <div className="alert alert-danger">
+            <p>¿Estás seguro de que quieres eliminar este elemento?</p>
+            <Button
+              onClick={() => {
+                eliminarElemento();
+                setMostrarConfirmacion(false);
+              }}
+              variant="contained"
+              color="error"
+            >
+              Sí
+            </Button>
+            <Button
+              onClick={() => setMostrarConfirmacion(false)}
+              variant="contained"
+              color="secondary"
+            >
+              Cancelar
+            </Button>
+          </div>
         )}
       </div>
     </div>
--- src/VerUsuario.js
+++ src/VerUsuario.js
@@ -1,61 +1,115 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import Button from '@mui/material/Button';
 
 function VerUsuario() {
   const navigate = useNavigate();
   const [usuarios, setUsuarios] = useState([]);
+  const [elementoAEliminar, setElementoAEliminar] = useState(null);
+  const [mostrarConfirmacion, setMostrarConfirmacion] = useState(false);
 
   useEffect(() => {
     // Obtener datos de usuarios
-    fetch('http://localhost:3000/usuarios', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: 'Bearer ' + localStorage.getItem('token'),
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => setUsuarios(data))
+    axios
+      .get('http://localhost:3000/usuarios', {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + localStorage.getItem('token'),
+        },
+      })
+      .then((response) => setUsuarios(response.data))
       .catch((error) => console.log(error));
   }, []);
 
+  // Función para eliminar un usuario
+  const eliminarUsuario = (id) => {
+    axios
+      .delete(`http://localhost:3000/usuarios/${id}`, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + localStorage.getItem('token'),
+        },
+      })
+      .then(() => {
+        setUsuarios(usuarios.filter((usuario) => usuario.usuarioId !== id));
+        setElementoAEliminar(null);
+        setMostrarConfirmacion(false);
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
-    <div className='container'>
+    <div className="container">
       <div>
         <h4>Listado de usuarios</h4>
         {usuarios.length === 0 && (
-          <h3 className='text-center alert alert-danger'>No hay datos para mostrar</h3>
+          <h3 className="text-center alert alert-danger">No hay datos para mostrar</h3>
         )}
         {usuarios.length > 0 && (
-          <table className='table table-bordered table-dark'>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Nombre</th>
-                <th>Email</th>
-                <th>Acciones</th>
-              </tr>
-            </thead>
-            <tbody>
-              {usuarios.length > 0 &&
-                usuarios.map((usuario) => (
-                  <tr key={usuario.usuarioId}>
-                    <td>{usuario.usuarioId}</td>
-                    <td>{usuario.nombre}</td>
-                    <td>{usuario.email}</td>
-                    <td>
-                      <button
-                        onClick={() => navigate(`/actualizaruser/${usuario.usuarioId}`)}
-                        className='btn btn-primary'
-                      >
-                        Editar
-                      </button>
-                      <button className='btn btn-danger'>Eliminar</button>
-                    </td>
-                  </tr>
-                ))}
-            </tbody>
-          </table>
+          <div style={{ maxHeight: '400px', overflowY: 'scroll' }}>
+            <table className="table table-bordered table-dark">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Nombre</th>
+                  <th>Email</th>
+                  <th>Acciones</th>
+                </tr>
+              </thead>
+              <tbody>
+                {usuarios.length > 0 &&
+                  usuarios.map((usuario) => (
+                    <tr key={usuario.usuarioId}>
+                      <td>{usuario.usuarioId}</td>
+                      <td>{usuario.nombre}</td>
+                      <td>{usuario.email}</td>
+                      <td>
+                        <Button
+                          onClick={() => navigate(`/actualizaruser/${usuario.usuarioId}`)}
+                          variant="contained"
+                          color="primary"
+                        >
+                          Editar
+                        </Button>
+                        <Button
+                          onClick={() => {
+                            setElementoAEliminar(usuario.usuarioId);
+                            setMostrarConfirmacion(true);
+                          }}
+                          variant="contained"
+                          color="error"
+                        >
+                          Eliminar
+                        </Button>
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+        {mostrarConfirmacion && (
+          <div className="alert alert-danger">
+            <p>¿Estás seguro de que quieres eliminar este usuario?</p>
+            <Button
+              onClick={() => {
+                eliminarUsuario(elementoAEliminar);
+                setMostrarConfirmacion(false);
+              }}
+              variant="contained"
+              color="error"
+            >
+              Sí
+            </Button>
+            <Button
+              onClick={() => setMostrarConfirmacion(false)}
+              variant="contained"
+              color="secondary"
+            >
+              Cancelar
+            </Button>
+          </div>
         )}
       </div>
     </div>
